fac6615741958f0d31a9f9ae66e51446bc53b8b0
Luis Guerrero Escoto <chimyfernando1992@gmail.com>
2025-04-17 00:19:15 -0700

    added card flip + center layout

--- card/playing-card.js
+++ card/playing-card.js
@@ -1,59 +1,84 @@
 class PlayingCard extends HTMLElement {
-    // Setting up Shadow DOM and render when added to the page
-    constructor() {
-        super();
-        // I am choosing to use Shadow DOM so that the styles and structure of this component are isolated.
-        // This means our <playing-card> will look the same no matter where it's used,
-        // and won't be affected by (or accidentally affect) styles from the rest of the app.  
-        // This in some sense makes the playing-card.css file useless.
-        this.attachShadow({ mode: 'open' });
-    }
+  // Setting up Shadow DOM and render when added to the page
+  constructor() {
+      super();
+      // I am choosing to use Shadow DOM so that the styles and structure of this component are isolated.
+      // This means our <playing-card> will look the same no matter where it's used,
+      // and won't be affected by (or accidentally affect) styles from the rest of the app.  
+      // This in some sense makes the playing-card.css file useless.
+      this.attachShadow({ mode: 'open' });
+  }
 
-    // This function runs when the component is added to the page
-    connectedCallback() {
-        this.render();
-    }
+  // This function runs when the component is added to the page
+  connectedCallback() {
+      this.render();
+  }
 
-    // For now, we are just going to render once, not worry about updating.
-    render() {
-        const rank = this.getAttribute('rank');
-        const suit = this.getAttribute('suit');
-        const faceUp = this.getAttribute('face-up') === 'true'; // "true" string -> boolean
+  // For now, we are just going to render once, not worry about updating.
+  render() {
+    const rank = this.getAttribute('rank');
+    const suit = this.getAttribute('suit');
+    const faceUp = this.getAttribute('face-up') === 'true';
 
-        // Building path to image 
-        const frontImgPath = `PNGCards/${rank}_of_${suit}.png`;
-        const backImgPath = `PNGCards/back_card.png`;
+    const frontImgPath = `PNGCards/${rank}_of_${suit}.png`;
+    const backImgPath = `PNGCards/back_card.png`;
 
-        const imgPath = faceUp ? frontImgPath : backImgPath;
+    this.shadowRoot.innerHTML = `
+      <style>
+        .card {
+          width: 150px;
+          height: 218px;
+          perspective: 1000px;
+          cursor: pointer;
+        }
 
-        this.shadowRoot.innerHTML = `
-        <style>
-          .card {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background: none;
-            box-shadow: none;
-            padding: 0;
-            margin: 0;
-            border-radius: 0;
-            overflow: visible;
-            }
-  
-          .card img {
-            max-width: 150px;
-            height: auto;
-            display: block;
-            }
-          }
-        </style>
-  
-        <div class="card">
-                <img src="${imgPath}" alt="${faceUp ? rank + ' of ' + suit : 'card back'}">
+        .card-inner {
+          width: 100%;
+          height: 100%;
+          position: relative;
+          transform-style: preserve-3d;
+          transition: transform 0.6s ease;
+        }
+
+        .card-inner.flip {
+          transform: rotateY(180deg);
+        }
+
+        .card-face {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          backface-visibility: hidden;
+          background-size: cover;
+          background-position: center;
+          border-radius: 8px;
+        }
+
+        .front {
+          background-image: url('${frontImgPath}');
+        }
+
+        .back {
+          background-image: url('${backImgPath}');
+          transform: rotateY(180deg);
+        }
+      </style>
+
+      <div class="card">
+        <div class="card-inner ${faceUp ? '' : 'flip'}">
+          <div class="card-face front"></div>
+          <div class="card-face back"></div>
         </div>
-        `;
-    }
+      </div>
+    `;
 
-    
+    // Add click to flip
+    const inner = this.shadowRoot.querySelector('.card-inner');
+    this.shadowRoot.querySelector('.card').addEventListener('click', () => {
+      inner.classList.toggle('flip');
+    });
+  }
+
+  
 }
 customElements.define('playing-card', PlayingCard);
